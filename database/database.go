08966954688728662c286bf756d3c925d5e95ae2
Mahdi Farzadi <mahdifarzadi78@gmail.com>
2021-09-29 16:43:34 +0330

    add gorm v1

--- database/database.go
+++ database/database.go
@@ -6,9 +6,14 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+
+	gormv1 "github.com/jinzhu/gorm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *gorm.DB
+
+var DBv1 *gormv1.DB
 
 func ConnectToDB(username string, password string, address string, port string, dbName string) (err error) {
 
@@ -16,6 +21,16 @@ func ConnectToDB(username string, password string, address string, port string, 
 	log.Println(dsn)
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	
+	return
+}
+
+func ConnectToDBv1(username string, password string, address string, port string, dbName string) (err error) {
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, address, port, dbName)
+	log.Println(dsn)
+
+	DBv1, err = gormv1.Open("mysql", dsn)
 	
 	return
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/pump-shop/backend-repository
 go 1.16
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jinzhu/gorm v1.9.16
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.12
 )
